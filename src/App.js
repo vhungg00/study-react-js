4e49b58bee4d20c0f4f87ee16d192b47cac78523
vhungg00 <81247920+vhungg00@users.noreply.github.com>
2021-06-11 22:36:22 +0700

    hùng push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const [Number, setNumber] = React.useState(0)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="jumbotron">
+  <h1 className="display-3">Demo state</h1>
+  <h3>{Number}</h3>
+ <button type="button" className="btn btn-primary" onClick={() => setNumber(Number +1 )}> Tăng </button>
+ <button type="button" className="btn btn-info"  onClick={() => setNumber(Number - 1 )}>Gỉam </button>
+</div>
+
     </div>
   );
 }
